f7e1b16693f9f4fbd0ea75b9c87df68bd59a6607
MoisesEastland1 <meastland977@gmail.com>
2026-02-20 21:31:12 -0600

    adding files

---
+++ src/item.js
@@ -0,0 +1,17 @@
+class Meal {
+  constructor(name, calories) {
+    this.id = Math.random().toString(16).slice(2);
+    this.name = name;
+    this.calories = calories;
+  }
+}//This is the end of Meal class
+
+class Workout {
+  constructor(name, calories) {
+    this.id = Math.random().toString(16).slice(2);
+    this.name = name;
+    this.calories = calories;
+  }
+}//this is the end of Workout class
+
+export {Meal, Workout};
